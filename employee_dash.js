a213030b8e9743f887af682bc57920b24749ee12
VamC <117295356+VamCgit21@users.noreply.github.com>
2023-09-15 00:03:50 +0530

    Add files via upload

---
+++ employee_dash.js
@@ -0,0 +1,20 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+  
+  function openSearch() {
+    document.getElementById("myOverlay").style.display = "block";
+  }
+  
+  function closeSearch() {
+    document.getElementById("myOverlay").style.display = "none";
+  }
+  
+  
+  
+  
+  
